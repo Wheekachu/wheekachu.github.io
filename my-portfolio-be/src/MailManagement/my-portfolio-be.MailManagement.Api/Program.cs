ad801ea36dd52cc361d9145403a2772aeac7fcca
Wheekachu <hamtorifrog@gmail.com>
2026-07-17 18:17:54 +0800

    deployment

--- my-portfolio-be/src/MailManagement/my-portfolio-be.MailManagement.Api/Program.cs
+++ my-portfolio-be/src/MailManagement/my-portfolio-be.MailManagement.Api/Program.cs
@@ -30,7 +30,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173", "http://localhost:3000") // Add your React URLs here
+                          //("http://localhost:5173", "http://localhost:3000")
+                          policy.WithOrigins("https://wheekachu.github.io/") // Add your React URLs here
                                 .AllowAnyHeader()
                                 .WithMethods(CorsConfiguration.AllowedMethods);
                       });
